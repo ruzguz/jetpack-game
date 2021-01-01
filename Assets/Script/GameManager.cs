31223d58c912c583a04fdd0f186c5d145673cb62
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-01 17:36:06 -0500

    Show pause menu when press puse button

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Singleton
+    public static GameManager sharedInstance;
+    
+    // UI vars
+    private 
+
+    void Awake() 
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Singleton validation
+        if (sharedInstance == null) 
+        {
+            sharedInstance = this;
+        } else 
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Pause game and show pause menu
+    public void PauseGame()
+    {
+        Debug.Log("PAUSE");
+    }
+
+}
