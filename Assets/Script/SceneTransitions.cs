cadd5316bb36153b01d0f38207184cd7798c050e
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-15 22:56:45 -0500

    Setup transition asteroid in both scenes

---
+++ Assets/Script/SceneTransitions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
