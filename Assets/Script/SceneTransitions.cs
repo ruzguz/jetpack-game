d658dff6f7fe8fedad006d59651e3345853b710a
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-15 23:20:15 -0500

    Added move asteroid (no change scene) animation

--- Assets/Script/SceneTransitions.cs
+++ Assets/Script/SceneTransitions.cs
@@ -6,10 +6,21 @@ using UnityEngine.SceneManagement;
 public class SceneTransitions : MonoBehaviour
 {
     public string sceneName;
+    private Animator transitionAnimator;
+
+    private void Awake() 
+    {
+        transitionAnimator = GetComponent<Animator>();
+    }
 
     public void ChangeScene()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void runTransitionAnimation()
+    {
+        transitionAnimator.SetTrigger("makeTransition");
     }
 
 }
