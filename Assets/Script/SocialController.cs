2469a73bd57df03742513befc044bbe1d9c2a261
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-21 23:26:24 -0500

    Add IG button for ruzguz profile

---
+++ Assets/Script/SocialController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialController : MonoBehaviour
+{
+
+    private string RUZGUZ_IG = "https://www.instagram.com/_ruzguz/";
+
+    public void OpenRuzguzIG()
+    {
+        Application.OpenURL(RUZGUZ_IG);
+    }
+
+}
