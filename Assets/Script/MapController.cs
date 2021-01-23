82d099d979a79ab0ceebf83b1a6dadfcc24bf149
Paulo Puleo <paulopuleo95@gmail.com>
2021-01-22 23:39:03 -0400

    Se agrega escalabilidad a MapController
    Se agrega la escalabilidad al Mapcontroller permitiendo que se puede agregar todos los bloques de obstaculos como hijos del mismo y comienzan a usarse en el juego sin necesidad de modificar el codigo

--- Assets/Script/MapController.cs
+++ Assets/Script/MapController.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    public GameObject Bloque1, Bloque2, Bloque3, Bloque4, Bloque5, Bloque6, Bloque7;
-    GameObject BloqueActual;
-    int i=0;                                                                                        //i es un indice auxiliar para ver que bloque sigue
+    int i;                                                                                          //i es un indice auxiliar para ver que bloque de obstaculoses el seleccionado
+    private void Start()                                                                            //Cuando inicia...
+    { 
+        i = Random.Range(0, transform.childCount);                                                  //Asigna un numero random a i entre 0 y el numero de hijos de MapController(los hijos son bloques de obstaculos)
+    }
+
     void Update()
     {
-        switch (i)                                                                                  //Activa el bloque de obstaculos segun i
-        {
-            case 0:
-                i++;
-                break;
-            case 1:
-                ActivarBloque(Bloque1);
-                break;
-            case 2:
-                ActivarBloque(Bloque2);
-                break;
-            case 3:
-                ActivarBloque(Bloque3);
-                break;
-            case 4:
-                ActivarBloque(Bloque4);
-                break;
-            case 5:
-                ActivarBloque(Bloque5);
-                break;
-            case 6:
-                ActivarBloque(Bloque6);
-                break;
-            case 7:
-                ActivarBloque(Bloque7);
-                break;
-        }
-
+        ActivarBloque(gameObject.transform.GetChild(i).gameObject);                                 //Activa el bloque con el indice i
     }
 
     void ActivarBloque(GameObject Bloque)                                                           //funcion para activa el bloque de obstaculos
@@ -44,9 +20,7 @@ public class MapController : MonoBehaviour
         if (Bloque.activeSelf == false)                                                             //Si el Bloque de obstaculos no esta activado...
             Bloque.SetActive(true);                                                                 //activa el bloque de obstaculos
         if (VerificarBloque(Bloque) == true)                                                        //si el bloque no tienen hijos activos...
-            i++;                                                                                    //Asigna un nuevo valor a i
-        if (i > 6)  
-            i = 0;
+            i = Random.Range(0, transform.childCount);                                              //Asigna un nuevo valor a i
     }
 
     bool VerificarBloque(GameObject Bloque)                                                         //Verificar si el bloque tiene algun hijo activo
