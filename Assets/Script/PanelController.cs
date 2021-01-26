b1c970cddb669cfcfe7ce1b8d4ca9445ad676b9d
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-25 21:18:12 -0500

    Add more details to info panel

---
+++ Assets/Script/PanelController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+
+    private Animator _panelAnimator;
+    
+    private void Awake() {
+        _panelAnimator = GetComponent<Animator>();
+    }
+
+    public void MoveIn()
+    {
+        _panelAnimator.SetBool("pauseGame", true);
+    }
+
+    public void MoveOut()
+    {
+        _panelAnimator.SetBool("pauseGame", false);
+    }
+    
+}
