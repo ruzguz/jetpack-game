1370f90cc44c504afd411627e77348b4f6e4fbf5
Rusben Guzman <rusbenguzman@gmail.com>
2021-01-26 22:50:49 -0500

    Add ReverseControls function when you press the button

---
+++ Assets/Script/ConfigManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigManager : MonoBehaviour
+{
+
+
+    // Vars
+    private int _controls;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("controls") == 0) 
+        {
+            PlayerPrefs.SetInt("controls", 1);
+        }
+
+        _controls = PlayerPrefs.GetInt("controls");
+        Debug.Log(_controls);
+    }
+
+    public void ReverseControls()
+    {
+        _controls*=-1;
+        PlayerPrefs.SetInt("controls", _controls);
+        Debug.Log(_controls);
+    }
+
+}
