a39b68d57381492d81202329bb0fffb0deb12e36
Paulo Puleo <paulopuleo95@gmail.com>
2021-02-02 15:18:00 -0400

    Update from main

---
+++ Assets/Script/SceneTransitions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+
+    // General vars
+    public bool makeTransition;
+    public string sceneName;
+    private Animator transitionAnimator;
+
+    // Delegate to run method when the scene is loaded 
+    private void OnEnable() 
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
+    {
+
+        if (makeTransition) 
+        {
+            Debug.Log("Scene Loaded");
+            runMoveAnimation();
+        }
+
+    }
+    private void OnDisable() 
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void Awake() 
+    {
+        transitionAnimator = GetComponent<Animator>();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void runTransitionAnimation()
+    {
+        transitionAnimator.SetTrigger("makeTransition");
+    }
+
+    public void runMoveAnimation() 
+    {
+        transitionAnimator.SetTrigger("move");
+    }
+
+}
---
+++ Assets/Script/SocialController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialController : MonoBehaviour
+{
+
+    private string INSTAGRAM_URL = "https://www.instagram.com";
+
+
+    public void OpenIG(string user)
+    {
+        string url = string.Format("{0}/{1}", INSTAGRAM_URL, user);
+        Debug.Log(url);
+        Application.OpenURL(url);
+    }
+
+}
